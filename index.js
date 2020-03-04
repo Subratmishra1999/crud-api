da11809c8df18cb1c5f751b63b9b1085d0585a3a
Subrat Mishra <subratmishra198@gmail.com>
2020-03-04 15:48:00 +0530

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+//Database Connection
+require('./config/database');
+
+// define a simple route
+var routers = require('./Router/router');
+app.use('/', routers);
+
+// listen for requests
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Server is listening on port " + PORT);
+});
